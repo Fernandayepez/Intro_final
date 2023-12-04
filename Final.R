9c2d79a3e5fd00392c8357dc6ae5c815bf3da111
Fernandayepez <148798489+Fernandayepez@users.noreply.github.com>
2023-12-04 12:55:51 -0500

    second test pass

--- Final.R
+++ Final.R
@@ -41,6 +41,37 @@ fit <- fitdistr(c_value_df$Result, densfun = "normal")
 mean_estimate <- fit$estimate["mean"]
 sd_estimate <- fit$estimate["sd"]
 
-# I thought it was normal now 
+#Normal after log transformation
 hist(c_value_df$Result, main = "Histogram of C Values", xlab = "C Values", ylab = "Frequency", col = "lightblue", border = "black")
 
+
+# function that gives us the adjacency matrix from our original edge 
+create_adjacency_matrix <- function(edges) {
+  # Find the number of nodes, to get the size of our matrix
+  nodes <- unique(as.vector(edges))
+  num_nodes <- length(nodes)
+  
+  # Create an empty adjacency matrix
+  adj_matrix <- matrix(0, nrow = num_nodes, ncol = num_nodes)
+  
+  # Recursively populate 
+  for (i in 1:nrow(edges)) {
+    #we look at the edge matrix and the right column are the from_nodes
+    from_node <- edges[i, 1]
+    #left columns
+    to_node <- edges[i, 2]
+    
+    if (from_node %in% nodes) {
+      #storing the first to_node as the right child, defined as 1
+      adj_matrix[from_node, to_node] <- 1 
+      #we remove from_node from the list
+      nodes <- nodes[nodes != from_node]
+    } else {
+      #if it appears again then we assign the remaining child to the left, defined as 2
+      adj_matrix[from_node, to_node] <- 2  
+    }
+  }
+  
+  return(adj_matrix)
+}
+  
--- test.R
+++ test.R
@@ -7,13 +7,31 @@ test_that("calculate_sum_squared_differences returns the expected result for a v
   # Input vector
   input_vector <- 1:10
   
-  # Expected result (you need to calculate or obtain this)
+  # Expected result 
   expected_result <- 0.1090909
     
-    # Actual result
-    actual_result <- calculate_sum_squared_differences(input_vector)
+  # Actual result
+  actual_result <- calculate_sum_squared_differences(input_vector)
   
   # Check if the actual result is equal to the expected result
   expect_equal(actual_result, expected_result)
 })
 
+test_that(" create_adjancency_matrix returns an expected matrix given an edge matrix", {
+ library(ape)
+  #set up needed 
+  original_tree <- read.tree(text = "((A, B),  (C,D));")
+  edges <- original_tree$edge
+  
+  #expected result
+  expected_result <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0), nrow = 7,
+  byrow = TRUE)
+  
+  #Actual result
+  actual_result <-create_adjacency_matrix(edges)
+  
+  #Check if the same 
+  expect_equal(actual_result, expected_result)
+  
+  })
