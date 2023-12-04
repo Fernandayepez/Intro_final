f49d5dad2acf82d70513c12ee0483f03e48e6c32
Fernandayepez <148798489+Fernandayepez@users.noreply.github.com>
2023-12-04 15:16:02 -0500

    just being cautious

--- test.R
+++ test.R
@@ -45,14 +45,10 @@ test_that("get_edge_matrix gives us the expected matrix given an adj", {
   
   #expected result
   expected_matrix <- matrix(c(5, 6, 6, 1, 6, 2, 5, 7, 7, 3, 7, 4), nrow = 6, byrow = TRUE)
-  print(expected_matrix)
-  print(class(expected_matrix))
-  
+
   #Actual result
   actual_result <-get_edge_matrix(adj_matrix)
-  print(actual_result)
-  print(class(actual_result))
-  
+
   #Check if the same 
   expect_equivalent(actual_result, expected_matrix)
   
